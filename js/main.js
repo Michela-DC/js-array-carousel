f2081db9204e9209a0487d5f0174c8a3534af5c9
Michela-DC <micheladelconte@outlook.com>
2022-02-11 19:25:03 +0100

    Added title and text for each slide

--- js/main.js
+++ js/main.js
@@ -30,32 +30,52 @@ let activeIndex = 0; // creo un indice che mi serve per spostare la classe activ
 for(let i = 0; i < items.length; i++){
     console.log(i);
 
-    const imgElement = ` <img class="thumbnail" src="${items[i]}" alt="">`;
+    // metodo per creare un elemento html usando createElement
+    const imgElement = document.createElement('img');
+    //inserisco il dentro all'elemento img l'immagine che dentro l'array items è nella posizione i del ciclo
+    imgElement.src = items[i];
+    // assegno la classe thumbnail all'elemento img che ho creato
+    imgElement.classList.add('thumbnail');
     console.log(imgElement);
-
+    // mi salvo in una variabile il container delle thumbnails che in html ha la classe thumbnails-container
     const thumbnailsContainer = document.querySelector(".thumbnails-container");
     console.log(thumbnailsContainer);
-    thumbnailsContainer.innerHTML += ` ${imgElement}`;
+    // dentro inserisco l'elemento <img> con dentro le immagini dell'array che ho creato prima e per farlo uso appen()
+    // Devo usare append () con document.createElement perchè sto creando un elemento html vero e proprio
+    thumbnailsContainer.append(imgElement);
 
-    // faccio la stessa cosa per le slide grandi
-    const imgSlide = ` <img class="slide" src="${items[i]}">`;
+    // --> Altro modo per creare un elemento html dal js (lo uso per le immagini grani dello slider):
+    // creo una variabile in cui ci inserisco il tag <img> con già la classe e dentro l'immagine dell'array item nella posizione i
+    const imgSlide = ` <img class="slide" src="${items[i]}">`; // in questo modo creo una stringa e non un elemento html vero e proprio
     console.log(imgSlide);
-
+    // recupero il wrapper delle slide dall'html
     const slideWrapper = document.querySelector('.slide-wrapper');
-    slideWrapper.innerHTML += ` ${imgSlide}`;
+    // dentro inserisco l'elemento <img> con dentro le immagini dell'array usando .innerHTML +=
+    // devo usare .innerHTML += perché con il metodo dei backtick ho creato una stringa e non un elemento html vero e proprio quindi sto effettivamente concatenando del testo
+    slideWrapper.innerHTML += `${imgSlide}`;
+
+    //Creare un titotlo e un <p> poi li inserisco dentro all'elemento html .img-info
+    const imgTitle = `<h3 class="img-title">${title[i]}</h3>`;
+    const imgText = `<p class="img-text">${text[i]}</p>`;
+    const imgInfo = document.querySelector('.img-info');
+    imgInfo.innerHTML += `${imgTitle} ${imgText}`;
+    console.log(imgTitle,imgText,imgInfo);
+    
 }
 
-// salvo l'array delle thumbnail e quello delle slide
+// salvo l'array delle thumbnail, delle slide e delle info
 const thumbnail = [...document.getElementsByClassName("thumbnail")]; // const thumbnail = document.getElementsByClassName("thumbnail"); se avessi fatto così mi ritornava una lista di nodi
-
 const slide = [...document.getElementsByClassName("slide")];
-console.log(thumbnail, slide);
+const slideTitle = [...document.getElementsByClassName("img-title")];
+const slideText = [...document.getElementsByClassName("img-text")];
+console.log(thumbnail, slide, slideTitle, slideText);
 
-// imposto la thumbnail e la slide attiva in base all'indice di partenza
+// imposto la thumbnail, la slide e le info attive in base all'indice di partenza
 thumbnail[activeIndex].classList.add('active'); // Se avessi fatto imgElement.classlist.add("active"); così non funziona perché imgElement é una stringa quindi mi sono creata l'array per prenderne gli elementi
-
 slide[activeIndex].classList.add('active-slide');
-console.log(thumbnail[activeIndex], slide[activeIndex]);
+slideTitle[activeIndex].classList.add('active-slide');
+slideText[activeIndex].classList.add('active-slide');
+console.log(thumbnail[activeIndex], slide[activeIndex],slideTitle[activeIndex],slideText[activeIndex]);
 
 // al click sui bottoni mi sposto su o giù di una immagine
 const btnUp = document.getElementById('btn-up');
@@ -69,18 +89,20 @@ btnUp.addEventListener('click', function(){
 
     // devo togliere la classe active dalla slide e dalla thumbnail correnti
     slide[activeIndex].classList.remove('active-slide');
-    console.log(slide[activeIndex]);
+    slideTitle[activeIndex].classList.remove('active-slide');
+    slideText[activeIndex].classList.remove('active-slide');
     thumbnail[activeIndex].classList.remove('active');
-    console.log(thumbnail[activeIndex]);
+    console.log(thumbnail[activeIndex], slide[activeIndex], slideTitle[activeIndex]);
 
     //incremento l'activeIndex per stabilire la nuova thumbnail e la nuova slide corrente
     activeIndex --;
 
     // devo aggiungere la classe active dalla slide e dalla thumbnail correnti
-    thumbnail[activeIndex].classList.add('active');
-    console.log(thumbnail[activeIndex]);
     slide[activeIndex].classList.add('active-slide');
-    console.log(slide[activeIndex]);
+    slideTitle[activeIndex].classList.add('active-slide');
+    slideText[activeIndex].classList.add('active-slide');
+    thumbnail[activeIndex].classList.add('active');
+    console.log(thumbnail[activeIndex], slide[activeIndex], slideTitle[activeIndex]);
 
 })
 
@@ -88,12 +110,17 @@ btnDown.addEventListener('click', function(){
     console.log('vai giù');
     
     slide[activeIndex].classList.remove('active-slide');
+    slideTitle[activeIndex].classList.remove('active-slide');
+    slideText[activeIndex].classList.remove('active-slide');
     thumbnail[activeIndex].classList.remove('active');
 
     activeIndex ++;
 
-    thumbnail[activeIndex].classList.add('active');
     slide[activeIndex].classList.add('active-slide');
+    slideTitle[activeIndex].classList.add('active-slide');
+    slideText[activeIndex].classList.add('active-slide');
+    thumbnail[activeIndex].classList.add('active');
+
 })
 
 
